cd55c7da61be3bfaaf5f06dd142c6131f3511b56
Harry1720 <22h1120002@ut.edu.vn>
2025-06-24 21:53:34 +0700

    ai love u

--- FE/constants/api.js
+++ FE/constants/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://192.168.100.184:3000/api"; // Replace with your actual IP address
-export const SOCKET_URL = "http://192.168.100.184:3000"; // Replace with your actual IP address
+export const API_URL = "http://192.168.1.6:3000/api"; // Replace with your actual IP address
+export const SOCKET_URL = "http://192.168.1.6:3000"; // Replace with your actual IP address
